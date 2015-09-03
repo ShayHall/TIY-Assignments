cc79dc51ce90a44780f092a43975b97afa737f42
Shay <shayhall@nccumc.org>
2015-09-02 23:08:00 -0400

    fromEnglish passed test

--- StringCalculator.js
+++ StringCalculator.js
@@ -5,25 +5,59 @@ test('fromEnglish: "zero" through "nine"', function(){
   expect(fromEnglish).to.exist;
   expect(fromEnglish("zero")).to.equal(0);
   expect(fromEnglish("one")).to.equal(1);
-  // more tests here, please...
+  expect(fronEnglish("two")).to.equal(2);
+  expect(fromEnglish("three")).to.equal(3);
+  expect(fromEnglish("four")).to.equal(4);
+  expect(fromEnglish("five")).to.equal(5);
+  expect(fromEnglish("six")).to.equal(6);
+  expect(fromEnglish("seven")).to.equal(7);
+  expect(fromEnglish("eight")).to.equal(8);
+  expect(fromEnglish("nine")).to.equal(9);
 }); // END test(fromEnglish)
- 
+
 /**
  * @param {String} word representing an English number
  * @return {Number} value corresponding to `word`
  */
 function fromEnglish(word){
-  // fill this in after writing tests...
+  if (word == "zero") {
+  word == 0;
+  } else if (word == "one"){
+    word == 1;
+  } else if (word == "two") {
+    word == 2;
+  } else if (word == "three") {
+    word == 3;
+  } else if (word == "four") {
+    word == 4;
+  } else if (word == "five") {
+    word == 5;
+  } else if (word == "six") {
+    word == 6;
+  } else if (word == "seven") {
+  word == 7;
+  } else if (word == "eight") {
+    word == 8;
+  } else if (word == "nine") {
+    word == 9
+  };
+};
 } // END fromEnglish
 
 test('plus: ("zero", "zero") through ("zero", "nine")', function(){
   expect(plus).to.exist;
   expect(plus("zero", "zero")).to.equal(0);
   expect(plus("zero", "one")).to.equal(1); // change just one thing!
-  expect(plus("zero", "two")).to.equal(FILL_ME_IN); // ha!
+  expect(plus("zero", "two")).to.equal(2); // ha!
+  expect(plus("zero", "three")).to.equal(3);
+  expect(plus("zero", "four")).to.equal(4);
+  expect(plus("zero", "five")).to.equal(5);
+  expect(plus("zero", "six")).to.equal(6);
+  expect(plus("zero", "seven")).to.equal(7);
+  expect(plus("zero", "eight")).to.equal(8);
+  expect(plus("zero", "nine")).to.equal(9);
   // write more tests like those...
 }); // END test(plus)
-
 /**
  * @param {String} A representing an English number word
  * @param {String} B representing an English number word
