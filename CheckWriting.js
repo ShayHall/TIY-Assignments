e3fa2872d92af00042376dc74c1f7430c0339741
Shay <shayhall@nccumc.org>
2015-09-06 12:14:12 -0400

    Passed 0-29, set up Beast

--- CheckWriting.js
+++ CheckWriting.js
@@ -50,65 +50,25 @@ test('toEnglish: `21` to `30`', function(){
  * @param {Number} value to convert to English number word
  * @return {String} representing `value` in English
  */
-function toEnglish(value){
-
-  switch(value) {
-    case 0:
-      return "zero";
-    case 1:
-      return "one";
-    case 2:
-      return "two";
-    case 3:
-      return "three";
-    case 4:
-      return "four";
-    case 5:
-      return "five";
-    case 6:
-      return "six";
-    case 7:
-      return "seven";
-    case 8:
-      return "eight";
-    case 9:
-      return "nine";
-    case 10:
-      return "ten";
-    case 11:
-      return "eleven";
-    case 12:
-      return "twelve";
-    case 13:
-      return "thirteen";
-    case 14:
-      return "fourteen";
-    case 15:
-      return "fifteen";
-    case 16:
-      return "sixteen";
-    case 17:
-      return "seventeen";
-    case 18:
-      return "eighteen";
-    case 19:
-      return "nineteen";
-    case 20:
-      return "twenty";
-
-    break;
-    default:
-      return false;
-  }
-  start simply and refactor...
+function toEnglish(value) {
+  var numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty"];
+  var value = numbers [value];
+  return value;
 } // END toEnglish
 
 test('BEAST MODE: toCheck', function(){
   assert.isFunction(toCheck);
 
+  assert.equal(toCheck(1), "one");
   assert.equal(toCheck(1.23), "one & 23/100s");
   assert.equal(toCheck(12.34), "twelve & 34/100s");
   assert.equal(toCheck(123.45), "one hundred twenty three & 45/100s");
   assert.equal(toCheck(1234.56), "one thousand, two hundred thirty four & 56/100s");
   // you might need to try some values in between...
 }); // END test(BEAST MODE)
+
+function toCheck(checkBook) {
+  var digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty"];
+  var numbers = digits [checkBook];
+  return numbers;
+};//END toCheck
