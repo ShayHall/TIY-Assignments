f33d3a8f3be57b3db9a63dee36b86a96767a4c9f
Shay <shayhall@nccumc.org>
2015-09-09 22:28:10 -0400

    After lots of research, made a small addition.

---
+++ NestedAccordion/js/scripts.js
@@ -0,0 +1,15 @@
+/**
+GOAL- Create a nested accordian website
+* Click on a `Header` and have a new block of text open up.
+* If you click again, the "accordian" will close"
+* If you click it again, the accordion closes
+
+TODO
+* Establish a connection between js and html page
+    -Run test like `alert` to see if link was established.
+* Connect "Oat cake tootsie roll" <h3> class = "cbp-nttrigger"> to javascript
+* We want an `onclick:` event handler--> document.getElementByClassName("cbp-nttrigger").onclick = //some function or method or object that will activate the HTML changes.
+*
+*
+*
+*
