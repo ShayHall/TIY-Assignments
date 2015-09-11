b3877491a6521b1cfd9b19caf5a61624034c4f25
Shay <shayhall@nccumc.org>
2015-09-11 10:26:29 -0400

    Working on an if then to activate or deactivate class=cbp-ntopen

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -1,20 +1,53 @@
-// *TODO
-// * When I click on a header...
- //selected the first element with class "cbp-nttrigger"
-  var heading = document.querySelector(".cbp-nttrigger"); //get an error saying:
+/*
+* WORKING CODE DO NOT DELETE!
+* WORKING CODE DO NOT DELETE!
 
-heading.addEventListener('click', function(){ //get an error saying: "Uncaught TypeError: Cannot read property 'addEventListener' of null" (anonymous function)	@	scripts.js:6
-  var tracer = console.log("Tracy McTracerton"); // Just checking to see if it works
-  document.querySelector("li").className= "cbp-ntopen";// * a new block of text open up by making function that will : 1) add	 class="cbp-ntopen" to first <li> (first child?) of <ul id="cbp-ntaccordion" class="cbp-ntaccordion">
-  heading.addEventListener('click', tracer); //Just checking to see if it works.
-
+var heading = document.querySelector(".cbp-nttrigger");
+heading.addEventListener('click', function(){
+document.querySelector("li").className= "cbp-ntopen";
 });
 
+* WORKING CODE DO NOT DELETE!
+* WORKING CODE DO NOT DELETE!
+*/
 
-// Align clicked header to top of page....
 
-// * When I click again on the same header...
-//
+var heading = document.querySelector(".cbp-nttrigger");
+var closeHeading = document.querySelector(".cbp-nttrigger");
+
+
+if (document.querySelector("li").className="cbp-ntopen" = ) {
+  return closeHeading.addEventListener('click', function(){
+  document.querySelector("li").className= "";
+  });
+
+  } else {
+
+  return heading.addEventListener('click', function(){
+  document.querySelector("li").className= "cbp-ntopen";;
+};
+
+
+
+
+// *TODO
+
+// * When I click on a header...
+//  //selected the first element with class "cbp-nttrigger"
+// var tracer = console.log("Tracy McTracerton"); // Just checking to see if it works
+// var heading = document.querySelector(".cbp-nttrigger"); //tried querySelectorAll, but it broke the code.
+// var closeHeading = document.querySelector(".cbp-nttrigger"); //made a identifier to undo open click event
+// /*if (class=cbp-ntopen" exists) {
+// closeHeading.addEventLister("click", function() {
+// document.querySelector("li").className="";});
+// */
+// heading.addEventListener('click', function(){
+// document.querySelector("li").className= "cbp-ntopen";
+// }); //tried to add useCapture parameter to addEventListener.. Didn't work.
+
+// close heading.addEventListener('click', document.querySelector("li").className= "";// * new block of text open by making function that will : 1) add class="cbp-ntopen" to 1st <li> (first child?) of <ul id="cbp-ntaccordion" class="cbp-ntaccordion">
+// });
+
 
 // * the block of text closes...
 //
