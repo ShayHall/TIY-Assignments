4b52f47d9abcfe2c9f8658b720a5e754a0bd917e
Shay <shayhall@nccumc.org>
2015-09-11 17:29:02 -0400

    Updated

---
+++ ViewModeSwitch/js/scripts.js
@@ -0,0 +1,16 @@
+/*
+
+* What events will I use?
+
+*1) Focus on the `button`
+    - How to `switch` the active button?
+    - Make a function that does that and invoke from REPL.
+    -Can you wire that up to a `click` listener?
+    -There are 2 buttons.
+*2) What caused the layout to shift when buttons are clicked?
+    (find reference to this?)
+    - Write a function to do that?
+    - Wire it up to a `click` event.
+    -Can you toggle between them?
+
+*/
