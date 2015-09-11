8870942df5207343b1890032d50421232d9ae1b7
Shay <shayhall@nccumc.org>
2015-09-10 21:00:02 -0400

    Set up better pseudocode

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -1,17 +1,26 @@
-/**
-GOAL- Create a nested accordian website
-* Click on a `Header` and have a new block of text open up.
-* If you click again, the "accordian" will close"
-* If you click it again, the accordion closes
+// *TODO
+// * When I click...
+//   <h3> class = "cbp-nttrigger">
 
-TODO
-* Establish a connection between js and html page
-    -Run test like `alert` to see if link was established.
-* Connect "Oat cake tootsie roll" <h3> class = "cbp-nttrigger"> to javascript
-* We want an `onclick:` event handler--> document.getElementByClassName("cbp-nttrigger").onclick = //some function or method or object that will activate the HTML changes.
-* Function and/or method(s) will : 1) reveal 'nested' <div> and align clicked header to top of page.
-*
-*Other things we might need:
-*
-*Interface, Events, Elements or nodes, Event handler, Event Target, Properties, Methods
-*
+// * on a header...
+//   `onclick:` event handler--> document.getElementByClassName("cbp-nttrigger").onclick = //some function or method or object that will activate the HTML changes.
+
+// * a new block of text open up...
+//   Function and/or method(s) will : 1) reveal 'nested' <div> and align clicked header to top of page.
+
+// * When I click again...
+//
+
+// * on the same header...
+//
+
+// * the block of text closes...
+//
+
+// * We want an
+// *
+
+// *Other things we might need: Interface, Events, Elements or nodes, Event handler, Event Target, Properties, Methods
+
+
+//
