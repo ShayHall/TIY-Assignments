4e5d2be1c09ebf42d0d2a841d0c40d0619768f03
Shay <shayhall@nccumc.org>
2015-09-14 12:08:28 -0400

    Took out useless code and redefined Identifiers better w/David's guidance

--- ViewModeSwitch/js/scripts.js
+++ ViewModeSwitch/js/scripts.js
@@ -1,17 +1,15 @@
 var buttons = document.querySelector("div #cbp-vm");
-var grid = document.querySelector("a.cbp-vm-icon.cbp-vm-grid");
-var list = document.querySelector("a.cbp-vm-icon.cbp-vm-list")
+var gridButton = document.querySelector("a.cbp-vm-icon.cbp-vm-grid");
+var listButton = document.querySelector("a.cbp-vm-icon.cbp-vm-list")
 
-grid.addEventListener('click', function(gridButton) {
-  var currentButton = gridButton.currentTarget;
-buttons.className = "cbp-vm-switcher cbp-vm-view-grid";
-grid.className = "cbp-vm-icon cbp-vm-grid cbp-vm-selected";
-list.className = "cbp-vm-icon cbp-vm-list"
+gridButton.addEventListener('click', function(event) {
+  buttons.className = "cbp-vm-switcher cbp-vm-view-grid";
+  gridButton.className = "cbp-vm-icon cbp-vm-grid cbp-vm-selected";
+  listButton.className = "cbp-vm-icon cbp-vm-list"
 });
 
-list.addEventListener('click', function (listButton) {
-  var currentButton = listButton.currentTarget;
+listButton.addEventListener('click', function (event) {
   buttons.className = "cbp-vm-switcher cbp-vm-view-list";
-  list.className = "cbp-vm-icon cbp-vm-list cbp-vm-selected";
-  grid.className = "cbp-vm-icon cbp-vm-grid";
+  listButton.className = "cbp-vm-icon cbp-vm-list cbp-vm-selected";
+  gridButton.className = "cbp-vm-icon cbp-vm-grid";
 });
