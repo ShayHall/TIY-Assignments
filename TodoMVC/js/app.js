7bf058a00eee9f4617ccaafafcf0977d71b6c049
Shay <shayhall@nccumc.org>
2015-09-14 21:02:14 -0400

    Pseudocode for first two UserStories written

--- TodoMVC/js/app.js
+++ TodoMVC/js/app.js
@@ -1,4 +1,40 @@
-//Using the HTML file `iindex.html` as the bones, CSS files and Javascript files are used to manipulate the code.
+//Using the HTML file `index.html` as the bones, CSS files and Javascript files are used to manipulate the code.
+
+
+/*
+** I can add a task to my todo list so that I can remember to do it later.
+
+	1) Attach an `addEventListener` to the `input` element of the `header` element of the `document` that...
+ 		A) Takes `change` as the type...
+			a) When `change` is triggered, a function is fired that...
+		B) Creates a new `li` element with the data from the header input is logged as a new text node:
+			a) Use the listener `document.createElement()` to create a new `li` element with a new `label` element inside it. (https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement)
+			b) Use the method `document.createTextNode` to store text from `input` element  (https://developer.mozilla.org/en-US/docs/Web/API/Document/createTextNode)
+
+** I can edit a task on my todo list so that I can correct typos.
+
+	1) Attach an `addEventListener` to the newly created `label` element that...
+ 		A) Takes `click` as the type...
+			a) When `click` is triggered, a function is fired that...
+		B) Edits the text:
+ 			a) Use the property `.innerHTML` on the `label` of the `li`  (https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML)
+
+** I can mark a task as complete so that I remember that I have done it.
+	1)
+
+** I can mark a completed task as incomplete so that I actually complete it this time.
+	1)
+
+** I can delete a task from my todo list so that I don't have to see it any more.
+	1)
+
+** I can delete all the completed tasks from my todo list so that I can clean up completed tasks.
+	1)
+
+** I can filter the list of tasks so that I can see just what tasks I have completed and what are left
+	1)
+
+*/
 
 (function (window) {
 	'use strict';
