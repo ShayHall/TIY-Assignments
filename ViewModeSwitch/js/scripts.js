c440cfb0b05f1e545222b3cd058fb60d474cf5a0
Shay <shayhall@nccumc.org>
2015-09-19 15:10:20 -0400

    Can't quite sync buttons yet.

--- ViewModeSwitch/js/scripts.js
+++ ViewModeSwitch/js/scripts.js
@@ -1,17 +1,34 @@
 /* View Mode Switch javascript code */
 
-var buttons = document.querySelector("div #cbp-vm");
-var gridButton = document.querySelector("a.cbp-vm-icon.cbp-vm-grid");
-var listButton = document.querySelector("a.cbp-vm-icon.cbp-vm-list")
+/**
+*
+* When `list` button is clicked...
+  -`list` button color changes to dark blue.
+  -`grid` button color changes to light gray.
+  -`div` with class `list` becomes "visible"
+  -`div` with class `grid` becomes "invisible"
+
+* When `grid` button is clicked...
+  -`grid` button color changes to dark blue.
+  -`list` button color changes to light gray.
+  -`div` with class `grid` becomes "visible"
+  -`div` with class `list` becomes "invisible"
+
+**/
+
+var toggle = document.querySelector("div #cbp-vm");
+var gridButton = document.querySelector("#grid");
+var listButton = document.querySelector("list");
 
 gridButton.addEventListener('click', function(event) {
-  buttons.className = "cbp-vm-switcher cbp-vm-view-grid";
+  console.log("BUBBLES!");
+  toggle.className = "cbp-vm-switcher cbp-vm-view-grid";
   gridButton.className = "cbp-vm-icon cbp-vm-grid cbp-vm-selected";
   listButton.className = "cbp-vm-icon cbp-vm-list"
 });
 
 listButton.addEventListener('click', function (event) {
-  buttons.className = "cbp-vm-switcher cbp-vm-view-list";
+  toggle.className = "cbp-vm-switcher cbp-vm-view-list";
   listButton.className = "cbp-vm-icon cbp-vm-list cbp-vm-selected";
   gridButton.className = "cbp-vm-icon cbp-vm-grid";
 });
