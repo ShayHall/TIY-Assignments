45704235eefc6be744041eec078c0acda0b0947e
Shay <shayhall@nccumc.org>
2015-09-25 22:06:45 -0400

    Toggle each h2 individually

--- NestedAccordion/js/main.js
+++ NestedAccordion/js/main.js
@@ -1,4 +1,19 @@
-$(".accord-trig").click(function(){
-  $(".sub-accordion").toggleClass("subtract-accordion");
-  console.log("still here");
+$(".accord-one").click(function(){
+  $(".sub-one").toggleClass("subtract-accordion");
+});
+
+$(".accord-two").click(function(){
+  $(".sub-two").toggleClass("subtract-accordion");
+});
+
+$(".accord-three").click(function(){
+  $(".sub-three").toggleClass("subtract-accordion");
+});
+
+$(".accord-four").click(function(){
+  $(".sub-four").toggleClass("subtract-accordion");
+});
+
+$(".accord-five").click(function(){
+  $(".sub-five").toggleClass("subtract-accordion");
 });
